89887eca44a23bef73135a72b850acf9c6509c43
Zemheri25 <omerzemheri14@gmail.com>
2022-05-26 02:46:12 +0300

    Django Create-Read-Update-Delete working added

---
+++ fscohort/forms.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from django import forms
+from .models import Student
+
+class StudentForm(forms.ModelForm):
+    
+    class Meta:
+        model = Student
+        fields = '__all__'
+        labels = {'first_name' : "Adınız", 'last_name' : 'Soyadınız', 'number' : "Numaranız"}
